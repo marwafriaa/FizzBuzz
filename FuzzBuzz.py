a8692378c50d22a08468553d347a67493f580de4
marwafriaa <friaamarwa14@gmail.com>
2019-02-26 16:30:48 +0100

    Update FuzzBuzz.py

--- FuzzBuzz.py
+++ FuzzBuzz.py
@@ -3,22 +3,25 @@
 
 # In[13]:
 
-
+# Fizzbuzz verifications and prints , the parameter nb is the number of iterations we want to get
 def fizzbuzz(nb):
     for i in range(nb):
+        # To verify if i is a maltiple of 3 and 5
         if verify_mult(i,3) and verify_mult(i,5):
             print("fizzbuzz")
             continue
+         # To verify if i is a maltiple of 3 
         elif verify_mult(i,3) :
             print("fizz")
             continue
+         # To verify if i is a maltiple of 5
         elif verify_mult(i,5):
             print("buzz")
             continue
-        
+        # Else we print i
         print(i)
     
-        
+ # Verify if the a is a multiple of b        
 def verify_mult(a,b):
      if a % b == 0:
         return True
@@ -31,7 +34,7 @@ def verify_mult(a,b):
 
 
 # In[14]:
-
+# main function to run to test
 def main():
     fizzbuzz(100)
 
