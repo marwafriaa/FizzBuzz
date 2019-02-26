2dad8d9fbc84ba71e7b0f43ecdc79ef0793ebba0
marwafriaa <friaamarwa14@gmail.com>
2019-02-26 16:17:10 +0100

    FuzzBuzz functions

---
+++ FuzzBuzz.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+def fizzbuzz():
+    for i in range(100):
+        if verify_mult(i,3) and verify_mult(i,5):
+            print("fizzbuzz")
+            continue
+        elif verify_mult(i,3) :
+            print("fizz")
+            continue
+        elif verify_mult(i,5):
+            print("buzz")
+            continue
+        
+        print(i)
+    
+        
+def verify_mult(a,b):
+     if a % b == 0:
+        return True
+     else:
+        return False
+
+    
+
+    
+
+
+# In[14]:
+
+
+fizzbuzz()
+
+
+# In[6]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
